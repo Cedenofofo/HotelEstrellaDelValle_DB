8f8918def9ada84fdea42b6bf1a97027ba315f7a
Cedenofofo <fofocedeno8@gmail.com>
2025-11-23 13:45:46 -0600

    Update restauracion.sql
    acomoda comentarios para sacar ss

--- backup/restauracion.sql
+++ backup/restauracion.sql
@@ -7,8 +7,10 @@ FROM DISK = 'C:\Backups\HotelEstrellaDelValle.bak' -- ¡RUTA DE TU ARCHIVO .BAK!
 
 -- Cláusulas MOVE: Indican la nueva ubicación de los archivos .mdf y .ldf
 WITH 
-    MOVE 'HotelEstrellaDelValle_Data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\HotelEstrellaDelValle_RESTORED.mdf', -- Ajusta la ruta a tu carpeta de DATA de SQL Server
-    MOVE 'HotelEstrellaDelValle_Log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\HotelEstrellaDelValle_RESTORED_Log.ldf', -- Ajusta la ruta a tu carpeta de DATA de SQL Server
+    MOVE 'HotelEstrellaDelValle_Data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\HotelEstrellaDelValle_RESTORED.mdf', 
+    -- Ajusta la ruta a tu carpeta de DATA de SQL Server
+    MOVE 'HotelEstrellaDelValle_Log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\HotelEstrellaDelValle_RESTORED_Log.ldf', 
+    -- Ajusta la ruta a tu carpeta de DATA de SQL Server
     REPLACE, -- Permite sobrescribir una base de datos existente con el mismo nombre (si usas el mismo nombre)
     STATS = 10;
 GO
